19c48dcc539762083eff7e455c57d31fc9e49300
Zev35 <javalearner35@gmail.com>
2022-10-18 14:00:10 -0500

    coomt4

--- src/main/java/tm/test1.java
+++ src/main/java/tm/test1.java
@@ -4,8 +4,8 @@ public class test1 {
 
 	public static void main(String[] args) {
 		System.out.println("ggg");
-		System.out.println("ggg");
-		System.out.println("ggg");
+		System.out.println("oo");
+		System.out.println("ttt");
 
 	}
 
